78395b50a1d4b4c4553840ab181ca2cfd44b5f1f
AlexALGA04 <alexalcala131204@gmail.com>
2025-04-23 10:29:39 +0200

    - Botones del modal de listas mas intuitivos

--- Films/wwwroot/js/home.js
+++ Films/wwwroot/js/home.js
@@ -3,6 +3,33 @@
 
     movieButtons.forEach(button => {
         button.addEventListener("click", function () {
+
+            const typeList = this.getAttribute("data-list-type");
+
+            const listButtons = document.querySelectorAll(".listButton");
+
+            const listTypes = {
+                "1": "Pendiente",
+                "2": "Completada",
+                "3": "Favorita",
+                "4": "Abandonada"
+            };
+
+            const typeListString = listTypes[typeList];
+
+            listButtons.forEach(btn => {
+                btn.style.backgroundColor = "";
+
+                const originalText = btn.value;
+                const iconHTML = btn.querySelector("i").outerHTML;
+                btn.innerHTML = `${iconHTML} ${originalText}`;
+
+                if (btn.value == typeListString) {
+                    btn.style.backgroundColor = "red";
+                    btn.innerHTML = `${iconHTML} Eliminar de ${originalText}`;
+                }
+            });
+
             const movieId = this.getAttribute("data-id");
 
             const hiddenInputs = document.querySelectorAll(".idFilm");
