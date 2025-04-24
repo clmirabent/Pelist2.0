09522aa6d566d869db6f2da12ba9c369356aae94
clmirabent <leomiracar@gmail.com>
2025-04-24 12:09:40 +0200

    pagina para ver los amigos, añadida validaciones en el UI de login y signup

--- Films/Controllers/AccountController.cs
+++ Films/Controllers/AccountController.cs
@@ -1,59 +1,61 @@
 using System.Security.Claims;
 using Films.Context;
-using Films.Models;
+
 using Films.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Films.Services;
+
 using Microsoft.EntityFrameworkCore;
-using Films.Context;
-using Films.Services;
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Films.Controllers;
 
-public class AccountController: Controller
+public class AccountController : Controller
 {
     private readonly FilmsDbContext _context;
 
-    private readonly ICloudinaryService _cloudinaryService;
-    
-    public AccountController(FilmsDbContext context, ICloudinaryService cloudinaryService)
+    public AccountController(FilmsDbContext context)
     {
         _context = context;
-        _cloudinaryService = cloudinaryService;
     }
-
 
     public async Task<IActionResult> Profile()
     {
-        if (!int.TryParse(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value, out var userId) || userId == null)
+        var id = GetUserIdFromClaims();
+        
+        if (id == null)
+        {
+            // Si no hay id en los claims, se define el mensaje para el popup
+            TempData["SweetAlertMessage"] = "Por favor, inicia sesión.";
             return RedirectToAction("Login", "Authentication");
-       
+        }
+
         //search for the user in database and their properties
         var user = await _context.Users
             .Include(u => u.Lists)
             .ThenInclude(l => l.FkIdTypeListNavigation)
             .Include(u => u.FriendFkIdFriendNavigations)
-            .FirstOrDefaultAsync(u => u.IdUser == userId);
+            .FirstOrDefaultAsync(u => u.IdUser == id);
 
         var typeLists = await _context.TypeLists.ToListAsync();
         if (user == null)
         {
-            return NotFound("Usuario no encontrado.");
+            TempData["SweetAlertMessage"] = "Por favor, inicia sesión.";
+            return RedirectToAction("Login");
         }
 
+        //var friends = await GetFriendsAsync(userId);
         var viewModel = new UserProfileViewModel
         {
             User = user,
-            TypeLists = typeLists
+            TypeLists = typeLists,
+            //Friends = friends,
+        };
 
-        }; 
-       
         return View(viewModel);
-        
-        
     }
+
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Logout()
@@ -64,6 +66,33 @@ public class AccountController: Controller
         return RedirectToAction("Index", "Home");
     }
 
-  
+   
+    public async Task<IActionResult> Friends()
+    {
+        var id = GetUserIdFromClaims();
+        if (id == null)
+        {
+            TempData["SweetAlertMessage"] = "Por favor, inicia sesión para ver a tus amigos.";
+            return RedirectToAction("Login", "Authentication");
+        }
+
+        var user = await _context.Users
+            .Include(n => n.FriendFkIdUserNavigations).Include(user => user.FriendFkIdFriendNavigations)
+            .Where(n => n.IdUser == id).SingleOrDefaultAsync();
+       
+        var realFriends = user.FriendFkIdFriendNavigations.ToList();
+
+        return View(realFriends);
+        
+    }
+   private int? GetUserIdFromClaims()
+    {
+        if (int.TryParse(User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value, out var userId))
+        {
+            return userId;
+        }
+
+        return null;
+    }
 }
 
